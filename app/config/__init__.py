717cfadc38a5273219cd435152c38c20a787ad06
Billbern <denis.abaidoo@gmail.com>
2021-09-25 09:59:34 +0000

    fixed environment variable

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -4,7 +4,7 @@ class Config(object):
     pass
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
     DEBUG = True
